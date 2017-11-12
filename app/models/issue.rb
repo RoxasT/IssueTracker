20b0121d15ae1173526b4b730bee713a3b331ce9
Aleix Balletbo <aleix.balletbo@est.fib.upc.edu>
2017-11-12 10:16:31 +0000

    Canvi de status implementat, amb botó i desplegable

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,4 +2,13 @@ class Issue < ApplicationRecord
     belongs_to :user
     belongs_to :assignee, class_name: 'User', foreign_key: :assignee_id
     validates :Title, :Type, :Priority, presence: true
+    def self.status
+        ["New", "Open", "On hold", "Resolved", "Duplicate", "Invalid", "Won't fix", "Closed"]
+    end
+    def self.type
+        ["Bug", "Enhancement", "Proposal", "Task"]
+    end
+    def self.priority
+        ["Trivial", "Minor", "Major", "Critical", "Blocker"]
+    end
 end
