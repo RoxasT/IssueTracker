1b9a25ada3cb4a1ca3b9e10b2e34553751157442
Guillem Castro <guillem.castro@hotmail.com>
2017-11-19 23:59:55 +0100

    Canviats nom parametres issues

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -18,15 +18,15 @@ class IssuesController < ApplicationController
     end
     
     if params.has_key?(:type)
-      @issues = @issues.where(type: params[:type])
+      @issues = @issues.where(Type: params[:type])
     end
     
     if params.has_key?(:priority)
-      @issues = @issues.where(priority: params[:priority])
+      @issues = @issues.where(Priority: params[:priority])
     end
     
     if params.has_key?(:status)
-      @issues = @issues.where(status: params[:status])
+      @issues = @issues.where(Status: params[:status])
     end
     
     if params.has_key?(:statusOpen)
