5e5a5cda6c00112136dca4242f23c2852864bbfb
Aleix Balletbo <aleix.balletbo@est.fib.upc.edu>
2018-01-06 21:57:40 +0000

    comprovació de l'assignee als serializers

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,6 +1,6 @@
 class Issue < ApplicationRecord
     belongs_to :user
-    belongs_to :assignee, class_name: 'User', foreign_key: :assignee_id
+    belongs_to :assignee, class_name: 'User', foreign_key: :assignee_id, optional: true
     has_many :comments, dependent: :destroy
     has_many :votes, dependent: :destroy
     has_many :watchers, dependent: :destroy
--- app/serializers/IssueSerializer.rb
+++ app/serializers/IssueSerializer.rb
@@ -2,11 +2,19 @@ class IssueSerializer < ActiveModel::Serializer
     attributes :id, :Title, :Description, :Type, :Priority, :Status, :Votes, :Watchers, :is_voted_by_current_user, :is_watched_by_current_user, :created_at, :updated_at, :attachment_file_name, :attachment_content_type, :attachment_file_size, :attachment_updated_at, :_links
     
     def _links
-        links = {
-            self: { href: "/issues/#{object.id}" },
-            creator: { href: "/users/#{object.user_id}", id: object.user_id, name: User.find(object.user_id).name },
-            assignee: { href: "/users/#{object.assignee_id}", id: object.assignee_id, name: User.find(object.assignee_id).name },
-        }
+        if object.assignee_id != nil
+            links = {
+                self: { href: "/issues/#{object.id}" },
+                creator: { href: "/users/#{object.user_id}", id: object.user_id, name: User.find(object.user_id).name },
+                assignee: { href: "/users/#{object.assignee_id}", id: object.assignee_id, name: User.find(object.assignee_id).name },
+            }
+        else
+            links = {
+                self: { href: "/issues/#{object.id}" },
+                creator: { href: "/users/#{object.user_id}", id: object.user_id, name: User.find(object.user_id).name },
+                assignee: { href: null, id: null, name: null },
+            }
+        end
     end
     
     def is_voted_by_current_user
